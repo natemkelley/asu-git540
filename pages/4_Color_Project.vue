ee4f3d4c920e984c424393ab94b6900491fb97da
nmkelley <natemkelley@gmail.com>
2019-10-16 13:32:04 -0700

    Created Color Project

---
+++ pages/4_Color_Project.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="section row">
+      <div class="col m12">
+        <h2>Summary</h2>
+        <p>The goal of this assignment was to create a magazine headmast using skills and concepts learned from class. In addition, we were to create the magazine with the inspiration of "the color blue". This could be interpreted any way we liked.</p>
+      </div>
+    </div>
+    <div class="section row">
+      <div class="col m10 s12">
+        <image-meta caption="" imageURL="magCover.png"></image-meta>
+      </div>
+    </div>
+    <div class="section row">
+      <div class="col m12 s12">
+        <h2>Inspiration</h2>
+        <p>The primary inspiration for my magazine come from the color wheel which puts blue and yellow as complimentary colors. I wanted the headmast to look like a fashion magazine similar to Vogue.</p>
+        <image-meta caption="Color Wheel" imageURL="http://static1.squarespace.com/static/577e85abc534a5d5bcf943d2/595556513a04116aa182b8be/59555666ebbd1a889f5d3072/1501077841369/?format=1500w"></image-meta>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ImageMeta from "~/components/ImageWithMeta";
+
+export default {
+  components: {
+    ImageMeta
+  },
+  mounted() {}
+};
+</script>
+
+<style>
+</style>
